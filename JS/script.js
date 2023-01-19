bb0b317344f8f34c7de1cc5d8aaaada6ecbecb4c
MM29942 <52143479+mm299420@users.noreply.github.com>
2023-01-19 11:03:51 +0100

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+
+done = true;
+function home() {
+    menu = document.getElementById("HomeMenu");
+    if (menu.style.display == "none") {
+        menu.style.display = "block";
+    } else {
+        menu.style.display = "none";
+    }
+
+}
