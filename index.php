e06218d43fc999e1ade9fc21075abdb0d7a5af47
MM29942 <52143479+mm299420@users.noreply.github.com>
2023-01-19 10:51:20 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="de" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+    <script src="JS/script.js"></script>
+    <link rel="icon" href="IMG/cicada.png" type="image/png">
+    <link rel="stylesheet" type="text/css" href="CSS/style.css">
+</head>
+    <body>
+        <?php
+        include "Templates/homeBTN.php";
+        include "Templates/HomeMenu.php";
+        ?>
+    </body>
+</html>
+
